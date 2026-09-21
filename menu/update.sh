cd945087faa21c7e4954409fc3254db5e1456706
𝗜𝗠𝗥𝗔𝗡 𝗧𝗘𝗖𝗛 <imranamadou278@gmail.com>
2026-09-21 21:10:37 +0100

    Edite

--- menu/update.sh
+++ menu/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-readonly SERVER_HOST="https://raw.githubusercontent.com/imrantech237/update/main"
+readonly SERVER_HOST="https://raw.githubusercontent.com/imrantech237/IMRAN-TECH-VPS/main"
 export LN='\e[34m'
 export BG='\e[44m'
 export NC='\e[0m'
